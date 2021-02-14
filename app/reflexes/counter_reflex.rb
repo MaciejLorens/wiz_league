fe4d529b9577586487dcbe7b633b3f18b5a214b4
Maciej Lorens <maciej.lorens@gmail.com>
2021-02-14 20:50:50 +0000

    add map table

--- app/reflexes/counter_reflex.rb
+++ app/reflexes/counter_reflex.rb
@@ -2,4 +2,10 @@ class CounterReflex < ApplicationReflex
   def increment
     @count = element.dataset[:count].to_i + element.dataset[:step].to_i
   end
+
+  def select
+    row = element.dataset[:row]
+    cell = element.dataset[:cell]
+    morph "##{element.id}", render(partial: 'home/cell', locals: { row: row, cell: cell, selected: 'selected' })
+  end
 end
