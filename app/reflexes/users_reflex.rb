37e13b6557362382dba410761b01e4793e9b58e8
Maciej Lorens <maciej.lorens@gmail.com>
2021-02-22 23:37:34 +0100

    working moving via cable_ready on grid

--- app/reflexes/users_reflex.rb
+++ app/reflexes/users_reflex.rb
@@ -2,23 +2,20 @@ class UsersReflex < ApplicationReflex
   delegate :current_user, to: :connection
 
   def move
-    current_hex = current_user.hex
+    current_hex = current_user.reload.hex
     current_hex.update(user_id: nil)
-    cable_ready["visitors"].morph(
-      selector: 'li#' + current_hex.id.to_s,
+    cable_ready["visitors"].inner_html(
+      selector: "#hex_#{current_hex.id}",
       html: render(partial: "home/hex_inner", locals: {hex: current_hex})
     )
 
-    hex = Hex.find(element.id)
+    hex = Hex.find(element.dataset.id)
     hex.update(user_id: current_user.id)
-    cable_ready["visitors"].morph(
-      selector: 'li#' + hex.id.to_s,
+    cable_ready["visitors"].inner_html(
+      selector: "#hex_#{hex.id}",
       html: render(partial: "home/hex_inner", locals: {hex: hex})
     )
 
-    # morph("#{hex.id}", render(partial: 'hex', locals: { hex: hex }))
-
-    # morph :nothing
     cable_ready["visitors"].broadcast
 
     morph :nothing
