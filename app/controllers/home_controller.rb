a950a09dc8f366dc082ded430f208840648423d8
Maciej Lorens <maciej.lorens@gmail.com>
2021-03-13 15:22:46 +0100

    login by buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, :except => [:sign_as]
+
   def index
     @range = 5
     @current_hex = current_user.hex
     @hexes = @current_hex.neighbors(@range)
+  end
+
+  def sign_as
+    Rails.logger.info "     ===== params: #{params[:id].inspect} ====="
+    sign_in_and_redirect :user, User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
+  post 'home/sign_as'
 end
